931f4fe148d232ab12deb0934554e0d5d8a6013c
Jane-Rui <48551225+Jane-Rui@users.noreply.github.com>
2024-11-08 21:21:34 +0800

    Update fetch.ts

--- server/utils/fetch.ts
+++ server/utils/fetch.ts
@@ -1,5 +1,8 @@
-import { $fetch } from "ofetch"
+import { ProxyAgent } from "undici";
+import { $fetch } from "ofetch";
 
+const proxyAgent = new ProxyAgent("http://localhost:3128");
+const data = await $fetch("https://icanhazip.com", { dispatcher: proxyAgent });
 export const myFetch = $fetch.create({
   headers: {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
